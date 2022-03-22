592d90ac02970ddc45ae738e6b69741fe6588de9
Minayimam <mina.yimam@go.minneapolis.edu>
2022-03-22 01:55:38 -0500

    intial commit

--- app/src/main/java/com/example/landscapeandportraitlayout/MainActivity.kt
+++ app/src/main/java/com/example/landscapeandportraitlayout/MainActivity.kt
@@ -1,37 +1,89 @@
 package com.example.landscapeandportraitlayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+
+const val EXTRA_SQUARE_SIZE = "com.example.landscapeandportraitlayout.SQUARE_SIZE"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var seekBar: SeekBar
+    private lateinit var seekBarLabel: TextView
+    private lateinit var showSquareButton: Button
 
-    private lateinit var seekbar: SeekBar
-    private lateinit var seekbarLabel: TextView
+    private val squareResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result -> handleSquareResult(result)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        seekbar = findViewById(R.id.seekbar)
-        seekbarLabel= findViewById(R.id.seekbarLabel)
+        seekBar = findViewById(R.id.seek_bar)
+        seekBarLabel = findViewById(R.id.seek_bar_label)
+        showSquareButton = findViewById(R.id.show_square_button)
 
-        val initialprogress = seekbar.progress
-        updatelabel(initialprogress)
+        val initialProgress = seekBar.progress
+        updateLabel(initialProgress)
 
-        seekbar.setOnSeekBarChangeListener( object: SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(seekbarComponet : SeekBar?, progress: Int, fromUser: Boolean) {
-               updatelabel(progress)
+        //todo add listener to update label as seekbar changes
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekbarComponent: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateLabel(progress)
             }
+
             override fun onStartTrackingTouch(p0: SeekBar?) {}
+
             override fun onStopTrackingTouch(p0: SeekBar?) {}
         })
 
+        showSquareButton.setOnClickListener{
+            showSquare()
+        }
+
+
     }
-    private fun updatelabel(progress: Int ){
-        seekbarLabel.text = getString(R.string.seekbar_value_message, progress)
+
+    private fun updateLabel(progress: Int){
+        seekBarLabel.text = getString(R.string.seekbar_value_message, progress)
+
+    }
+    private fun showSquare(){
+
+//        val showSquareIntent = Intent(this, SquareActivity::class.java)
+//        showSquareIntent.putExtra(EXTRA_SQUARE_SIZE, seekBar.progress)
+////        startActivity(showSquareIntent)
+//        squareResultLauncher.launch(showSquareIntent)
+
+        Intent(this, SquareActivity::class.java).apply {
+            putExtra(EXTRA_SQUARE_SIZE, seekBar.progress)
+            squareResultLauncher.launch(this)
+        }
+    }
+
+    private fun handleSquareResult(result: ActivityResult){
+        // display result to user
+        if(result.resultCode == RESULT_OK){
+            val intent = result.data
+            val tapped = intent?.getBooleanExtra(EXTRA_TAPPED_INSIDE_SQUARE, false) ?: false
+            val message = if(tapped) {
+                getString(R.string.tapped_square)
+            } else {
+                getString(R.string.missed_square)
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+        } else if (result.resultCode == RESULT_CANCELED){
+            Toast.makeText(this, getString(R.string.did_not_try), Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
